1a635cdb2a5d67fb63c76d52d52d46688be1289a
mireiaGomariz <mireiapgomariz@gmail.com>
2020-03-05 20:41:25 +0100

    Change dropdown and navbar style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/navbar';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 function App() {
   return (
     <div className="App">
-      
       < Navbar />
 
     </div>

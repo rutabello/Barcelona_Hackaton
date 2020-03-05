139d00f4ce736e6c4fdd1e7889d8235d2af13105
Sandra <alecsandra.serediuc@gmail.com>
2020-03-05 20:12:01 +0100

    Added the footer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/navbar';
-
+import Footer from './components/Footer/Footer'
 function App() {
   return (
     <div className="App">
       
-      < Navbar />
-
+      <Navbar />
+      <Footer />
     </div>
   );
 }

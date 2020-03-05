ef0479dd243c12c88ce4db8d0f084691066cfe65
Julie Brosse <privatejuliedev@gmail.com>
2020-03-05 21:33:53 +0100

    everything works with the game backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SmallSquare from './components/SmallSquare'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class BigBoard extends React.Component {
+  state = {
+    squareIndex: null,
+    clickedIndex: false,
+    counter: 0,
+    smallSquaresArray: ["", "", "", "", "", "", "", "", ""]
+  }
+  
+  delay = 1000;
+
+  chooseSmallSquare = () => {
+    let randomIndex = Math.floor(Math.random() * this.state.smallSquaresArray.length);
+    this.setState ({
+      squareIndex: randomIndex
+    })
+  }
+
+  onEndTimer = () => {
+    if(this.state.clickedIndex === true){
+      this.setState({
+        clickedIndex: false, 
+        counter: this.state.counter + 1
+      })
+      this.delay = this.delay - 50;
+      console.log("success")
+      this.onClickStart()
+    } else {
+      console.log("finished")
+    }
+  }
+
+  itemClicked = () => {
+    this.setState({
+      clickedIndex: true
+    })
+  }
+
+  onClickStart = () => {
+    // Set 1st random index
+    this.chooseSmallSquare()
+    // Update random index each 2s
+    //If everything is false do the set Interval + count + 1. Else stop the game + alert with counter    
+    setTimeout(this.onEndTimer,
+      this.delay)
+  }
+
+
+  restartCounter = () => {
+    this.setState ({
+      counter: 0
+    })
+    this.delay = 1000
+  }
+
+    render() {
+      return ( 
+        <div id="board">
+          { 
+            this.state.smallSquaresArray.map((x, index) => 
+            //turns show to true if index of the smallsquare == the squareIndex rendered by the chooseSmallSquare function
+                <SmallSquare show={index === this.state.squareIndex} itemClicked={this.itemClicked}/>
+            )}
+            <p>{this.state.counter}</p>
+            <button onClick={this.onClickStart}>START</button>
+            <button onClick={this.restartCounter}>RESET THE RESULT</button>
+        </div>
+      )
+    }
 }
-
-export default App;
---
+++ src/logica.js
@@ -0,0 +1,20 @@
+// in App : return <GeneralSquare component />
+// GeneralSquare : return a map (of GenralSquare.length) of smallSquares
+// smallSquarecomponent : state : true / false
+
+// aPP : counter
+//generalSquare = Array of small squares number
+// every 1sc/0.5sc/0.25sc, a small square appear. appearing small square = random(generalSquare)
+// > the smallSquare randomes > its state changes from false (default) to true
+
+// the previous false disappear when time is over
+
+//dans SmallSquare component : Onclick :
+// SmallSquare.state.display = false ? return counter : return counter + 1 and it changes back to false
+
+// everytime
+
+
+// put the pointer as a hand
+// change your team >> the name of your team will be returned in the announcement of the result
+// choose the team you want to destroy >> the name will appear in the announcement of the result

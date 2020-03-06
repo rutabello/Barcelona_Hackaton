3047614a080135bd8925903c144a10cdec90b6c6
Sandra <alecsandra.serediuc@gmail.com>
2020-03-06 04:23:13 +0100

    Added the main page components with the link,router for the games

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Footer from './components/Footer/Footer'
 import AppCapitals from './components/Games/ChoseCapitals/appCapitals';
 import ButtonUserLogged from './components/ButtonUserLogged/ButtonUserLogged'
 import UserProfile from './components/UserProfile/userprofile'
-import DropdownCategories from './components/DropdownCategories/dropdown'
+import DropdownCategories from './components/DropdownCategories/dropdown';
+import { Switch, Route, Link} from 'react-router-dom';
+import ButtonGames from './components/ButtonGames/buttonGames';
+import ClickKill from './components/Games/ClickKill/click'
 
 const UserExample = {
   name :"Pepito ScrumMaster",
   img: "https://avatars3.githubusercontent.com/u/59797566?s=460&v=4",
-  mail: "amamama@dkdkdkd.com",
+  mail: "amamama@hola.com",
   age: '28',
   points: 20
 
@@ -30,12 +33,22 @@ class App extends Component{
   render(){
   return (
     <div className='App'>
-      
+    
       <Navbar />
       <ButtonUserLogged userHere={this.simulateUserLogged} />
       <UserProfile userprof={this.state.user}/>
       <DropdownCategories />
-      <AppCapitals/>
+      <Switch>
+      <Route exact path ='/'>
+      <ButtonGames />
+      </Route>
+      <Route path='/cityplay'>
+      <AppCapitals/> 
+      </Route>
+      <Route path='/tacleclick'>
+      <ClickKill /> 
+      </Route>
+      </Switch>
       <Footer />
 
 
---
+++ src/components/Games/ClickKill/click.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ClickKill =()=>{
+    return(
+        <h1>Hello!</h1>
+    )
+}
+export default ClickKill
